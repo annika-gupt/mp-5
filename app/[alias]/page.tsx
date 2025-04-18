d9b639866914cfe097b80fce8cfab2957079b7a0
annika <annigupt@bu.edu>
2025-04-18 13:07:56 -0400

    Fixed parameters for RedirectPage

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -1,9 +1,9 @@
 import getCollection, { URL_COLLECTION } from '@/db';
 import { redirect } from 'next/navigation';
 
-export default async function RedirectPage({ params }: { params: { alias: string } }) {
+export default async function RedirectPage({ params }: { params: Promise<{ alias: string }>;}) {
     const collection = await getCollection(URL_COLLECTION); // connects to database and grabs url collection in mongodb
-    const record = await collection.findOne({ alias: params.alias }); // searches documents for alias
+    const record = await collection.findOne({ alias: (await params).alias }); // searches documents for alias
 
     if (record?.url) { // checks whether record exists and has url property
         redirect(record.url); // if alias is found, it redirects user to matching URL using redirect()
